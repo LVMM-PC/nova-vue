382cb00d586d231388676d2dd4ff462c2a6b3233
em2046 <em2046@qq.com>
2020-02-28 18:32:25 +0800

    使用文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: 'Nova Vue',
+  description: '前端组件库 Vue 实现',
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/LVMM-PC/nova-vue' }
+    ],
+    sidebar: [
+      '/',
+      '/getting-started/',
+      '/alert/',
+      '/select/',
+      '/radio/',
+      '/checkbox/',
+      '/date-picker/',
+      '/calendar/',
+      '/autocomplete/'
+    ]
+  }
+};
